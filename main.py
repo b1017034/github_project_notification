6684d78e6f5327a87cd079f70dae1a97734ae263
中村　朝陽 <>
2019-05-26 07:43:32 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    html = '<html><title>welcome</title>'
+    html = html + '<body>welcome</body></html>'
+    return html
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
