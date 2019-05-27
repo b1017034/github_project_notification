c5d1daf794f8f41ede75956f55f77526b1df2b4e
中村　朝陽 <>
2019-05-27 13:39:42 +0900

    ヤバみ

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
+def index():
     html = '<html><title>welcome</title>'
     html = html + '<body>welcome</body></html>'
     return html
