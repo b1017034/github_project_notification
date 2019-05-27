95b474f9fa4d5179f17760ab204ebcd53061f623
中村　朝陽 <>
2019-05-27 15:23:22 +0900

    create_card

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 import os
 from flask import Flask, request, jsonify
+from slacker import Slacker
 import json
 
 app = Flask(__name__)
+slack = Slacker(os.environ["API_TOKEN"])
 
 @app.route('/')
 def index():
@@ -19,9 +21,17 @@ def card():
     if request.method == 'POST':
         data = request.get_json()
         print(data)
-        print(data['zen'])
+        if data['action']:
+            slack.chat.post_message('#test1', create_card(data))
         return jsonify(res='ok')
     return jsonify(res='505')
+
+
+def create_card(json):
+    name = json['sender']['login']
+    card_name = json['project_card']['note']
+    return "created: " + card_name + "\n" + "by: " + name
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
