585334fb136673425bc336fde0a730fcc145d789
中村　朝陽 <>
2019-05-27 13:54:56 +0900

    port設定

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ def index():
     html = html + '<body>welcome</body></html>'
     return html
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
