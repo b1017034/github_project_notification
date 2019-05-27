262619cb0d7adbd9aad3341d3338d60f62ef64cd
中村　朝陽 <>
2019-05-27 14:05:20 +0900

    request

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ def index():
     html = '<html><title>welcome</title>'
     html = html + '<body>welcome</body></html>'
     return html
+
+@app.route('/project_card', methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return"GET"
+    else:
+        print(request)
+        return request
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
